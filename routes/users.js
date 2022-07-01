f3511a342f16f6cb2d1f0b4b06367a85d2f76bf4
jaedalzaber <jaedalzaber@gmail.com>
2022-07-01 21:35:06 +0600

    router 'api/' added

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/server', (req, res, next)=>{
+router.get('/api/server', (req, res, next)=>{
   res.send('This is from express backend!');
 })
 
