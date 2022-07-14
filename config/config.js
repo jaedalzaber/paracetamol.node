53e29744811bbe9f4650a1adcb2f463a0e11047f
jaedalzaber <jaedalzaber@gmail.com>
2022-07-14 17:46:29 +0600

    fallback

--- config/config.js
+++ config/config.js
@@ -1,7 +1,13 @@
 module.exports = {
-    secretKey: '12345-67890-09876-54321',
-    
-    TOKEN_EXPIRATION_IN_SECONDS : 3600,
-    REFRESH_TOKEN_EXPIRATION_IN_SECONDS : 86400,
- }
- 
+	secretKey: '12345-67890-09876-54321',
+
+	TOKEN_EXPIRATION_IN_SECONDS: 3600,
+	REFRESH_TOKEN_EXPIRATION_IN_SECONDS: 86400,
+
+	mongoUrl: 'mongodb+srv://jaedalzaber:ASDqwe123@cluster.ej7ug.mongodb.net/paracetamoldb?retryWrites=true&w=majority',
+	FACEBOOK_APP_ID: '742589700408925',
+	FACEBOOK_APP_SECRET: 'b48ed927c5f74d165720340a1beb23e3',
+	GOOGLE_APP_ID: '218282197466-eksqappd9rh1j2uog7b77m43cj63qpav.apps.googleusercontent.com',
+	GOOGLE_APP_SECRET: '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',
+	
+}
